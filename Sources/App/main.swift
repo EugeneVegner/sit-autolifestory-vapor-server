926b6d352af770455c8adf80cb1ad2119fae60a0
Eugene Vegner <eugene.vegner@gmail.com>
2017-04-09 17:44:50 +0300

    New proj

---
+++ Sources/App/main.swift
@@ -0,0 +1,18 @@
+import Vapor
+import VaporMongo
+
+let drop = Droplet()
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+drop.resource("posts", PostController())
+
+let mongo = try VaporMongo.Provider(database: "alsmongo", user: "admin", password: "admin")
+drop.addProvider(mongo)
+
+
+drop.run()
